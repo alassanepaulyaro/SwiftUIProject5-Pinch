ff7de000b4268af934d826b0d3374ca437eb3c52
Paul Yaro <alassane-paul.yaro@outlook.fr>
2024-12-04 09:44:25 +0100

    setup

---
+++ SwiftUIProject5-Pinch/Screen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIProject5-Pinch
+//
+//  Created by Yaro Paul  on 04/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
